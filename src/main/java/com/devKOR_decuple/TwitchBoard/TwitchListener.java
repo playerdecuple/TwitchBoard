aeca4167210dcf180bb979f72fc6bfa0c6c83e53
playerdecuple <playerdecuple@gmail.com>
2020-12-21 07:06:28 +0900

    Update keys

--- src/main/java/com/devKOR_decuple/TwitchBoard/TwitchListener.java
+++ src/main/java/com/devKOR_decuple/TwitchBoard/TwitchListener.java
@@ -10,16 +10,18 @@ import com.gikk.twirk.types.twitchMessage.TwitchMessage;
 import com.gikk.twirk.types.users.TwitchUser;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import static com.devKOR_decuple.TwitchBoard.Core.ConfigurationHelper.*;
+import static com.devKOR_decuple.TwitchBoard.Core.EasyEqual.*;
 
 public class TwitchListener implements TwirkListener {
 
     private List<TwitchUser> userList = new ArrayList<>();
-    private final Integer[] keyboardVotes = new Integer[26];
+    private final Integer[] keyboardVotes = new Integer[565];
 
     public TwitchListener() throws AWTException {
 
@@ -60,10 +62,10 @@ public class TwitchListener implements TwirkListener {
     private int getKeyboardVoteResult() {
 
         if (verbose()) {
-            System.out.println("조회 완료 : [KEY] " + (char) (getArrayIndexOfMaxValue(keyboardVotes) + 65) + "(" + getArrayIndexOfMaxValue(keyboardVotes) + "), [VOTES] " + getMaxValue(keyboardVotes));
+            System.out.println("조회 완료 : [KEY] " + (char) (getArrayIndexOfMaxValue(keyboardVotes)) + "(" + getArrayIndexOfMaxValue(keyboardVotes) + "), [VOTES] " + getMaxValue(keyboardVotes));
         }
 
-        return getArrayIndexOfMaxValue(keyboardVotes) + 65;
+        return getArrayIndexOfMaxValue(keyboardVotes);
 
     }
 
@@ -104,21 +106,145 @@ public class TwitchListener implements TwirkListener {
     @Override
     public void onPrivMsg(TwitchUser sender, TwitchMessage message) {
 
-        if (message.getContent().length() == 1) {
+        /*
+         * StreamerKey| Array Index          | Type    | ChatKey                  | Checked
+         * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ [ Basic ]
+         * A ~ Z      : Array Index 0 ~ 25   : Type 0  : A ~ Z                    : [x]
+         * 0 ~ 9      : Array Index 26 ~ 35  : Type 1  : 0 ~ 9                    : [x]
+         * Ins ~ PgDn : Array Index 49 ~ 55  : Type 3  : Ins ~ PgDn               : [x]
+         * ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ [ Utility ]
+         * Escape     : Array Index 56       : Type 4  : Esc                      : [x]
+         * Tab        : Array Index 57       : Type 5  : Tab                      : [x]
+         * LShift     : Array Index 58       : Type 6  : SH, Shift                : [x]
+         * LCtrl      : Array Index 59       : Type 7  : CT, Ctrl                 : [x]
+         * LAlt       : Array Index 60       : Type 8  : AL, Alt                  : [x]
+         * Space bar  : Array Index 61       : Type 9  : SP, SpaceBar             : [x]
+         * BackSpace  : Array Index 62       : Type 10 : BS, BackSpace            : [x]
+         * Return     : Array Index 63       : Type 11 : Return, Enter, RT, EN    : [x]
+         * Arrow Key  : Array Index 64 ~ 68  : Type 15 : <, +, _, >               : [x]
+         * Mouse      : Array Index 69 ~ 73  : Type 16 : <<, ++, __, >>           : [x]
+         */
 
-            if (Pattern.matches("[a-zA-Z]", message.getContent())) {
+        // 넘락 해제해야 돼네 아 ㅡㅡ
+        // (It works when unlocked 'num lock'.)
+
+        String msg = message.getContent();
+
+        if (msg.length() == 1) {
+
+            if (Pattern.matches("[a-zA-Z]", msg)) {
 
                 if (userExistsInList(sender.getUserID())) return;
 
-                int voteKey = message.getContent().toUpperCase().charAt(0) - 65;
+                int voteKey = msg.toUpperCase().charAt(0);
                 keyboardVotes[voteKey]++;
                 userList.add(sender);
 
-                if (verboseAll()) System.out.println("키 받아옴 : [KEY] " + message.getContent().toUpperCase() + "(" + voteKey + "), [NOW VOTES] " + keyboardVotes[voteKey]);
+                if (verboseAll())
+                    System.out.println("키 받아옴 : [KEY] " + msg.toUpperCase() + "(" + voteKey + "), [NOW VOTES] " + keyboardVotes[voteKey]);
+
+            }
+
+            if (Pattern.matches("[0-9]", msg)) {
+
+                if (userExistsInList(sender.getUserID())) return;
+
+                int voteKey = msg.charAt(0);
+                keyboardVotes[voteKey]++;
+                userList.add(sender);
+
+                if (verboseAll())
+                    System.out.println("키 받아옴 : [KEY] " + msg.toUpperCase() + "(" + voteKey + "), [NOW VOTES] " + keyboardVotes[voteKey]);
 
             }
 
         }
+
+        boolean isCorrectKey = false;
+        int voteKey = 3;
+
+        if (eq(msg, "Ins", "Insert")) {
+            voteKey = KeyEvent.VK_INSERT;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "Home", "Hm")) {
+            voteKey = KeyEvent.VK_HOME;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "PgUp", "PageUp", "Page Up")) {
+            voteKey = KeyEvent.VK_PAGE_UP;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "Del", "Delete")) {
+            voteKey = KeyEvent.VK_DELETE;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "End")) {
+            voteKey = KeyEvent.VK_END;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "PgDn")) {
+            voteKey = KeyEvent.VK_PAGE_DOWN;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "Esc", "Escape")) {
+            voteKey = KeyEvent.VK_ESCAPE;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "Tab")) {
+            voteKey = KeyEvent.VK_TAB;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "SH", "Shift")) {
+            voteKey = KeyEvent.VK_SHIFT;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "CT", "Ctrl", "Control")) {
+            voteKey = KeyEvent.VK_CONTROL;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "AL", "Alt", "Alternative")) {
+            voteKey = KeyEvent.VK_ALT;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "SP", "Space", "Spacebar", "Space bar")) {
+            voteKey = KeyEvent.VK_SPACE;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "BS", "BA", "BackSpace", "Back Space", "Erase")) {
+            voteKey = KeyEvent.VK_BACK_SPACE;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "RT", "RTN", "EN", "ENT", "Enter", "Return")) {
+            voteKey = KeyEvent.VK_ENTER;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "+")) {
+            voteKey = KeyEvent.VK_UP;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "-")) {
+            voteKey = KeyEvent.VK_DOWN;
+            isCorrectKey = true;
+        }
+        if (eq(msg, "<")) {
+            voteKey = KeyEvent.VK_LEFT;
+            isCorrectKey = true;
+        }
+        if (eq(msg, ">")) {
+            voteKey = KeyEvent.VK_RIGHT;
+            isCorrectKey = true;
+        }
+
+        if (!isCorrectKey) return;
+        if (userExistsInList(sender.getUserID())) return;
+
+        keyboardVotes[voteKey]++;
+        userList.add(sender);
+
+        if (verboseAll())
+            System.out.println("키 받아옴 : [KEY] " + msg.toUpperCase() + "(" + voteKey + "), [NOW VOTES] " + keyboardVotes[voteKey]);
+
 
     }
 
